f7e2d5d003d9c48343130e1ea2881b7d69f787a7
Krtofkaa <145643745+Krtofkaa@users.noreply.github.com>
2024-05-16 09:17:29 +0300

    PABEIGTS KRASAINS

--- kalkulators.py
+++ kalkulators.py
@@ -3,6 +3,7 @@ from tkinter import*
 mansLogs=Tk()
 mansLogs.title('Kalkulators')
 
+
 def btnClick(number):
     current=e.get()#nolasa skaitli
     e.delete(0,END)#nodzēšsss
@@ -44,23 +45,23 @@ def notirit():
 
 
 
-
-btn0=Button(mansLogs,text='0',padx='40',pady='20',command=lambda:btnClick(0))
-btn1=Button(mansLogs,text='1',padx='40',pady='20',command=lambda:btnClick(1))
-btn2=Button(mansLogs,text='2',padx='40',pady='20',command=lambda:btnClick(2))
-btn3=Button(mansLogs,text='3',padx='40',pady='20',command=lambda:btnClick(3))
-btn4=Button(mansLogs,text='4',padx='40',pady='20',command=lambda:btnClick(4))
-btn5=Button(mansLogs,text='5',padx='40',pady='20',command=lambda:btnClick(5))
-btn6=Button(mansLogs,text='6',padx='40',pady='20',command=lambda:btnClick(6))
-btn7=Button(mansLogs,text='7',padx='40',pady='20',command=lambda:btnClick(7))
-btn8=Button(mansLogs,text='8',padx='40',pady='20',command=lambda:btnClick(8))
-btn9=Button(mansLogs,text='9',padx='40',pady='20',command=lambda:btnClick(9))
-btnSask=Button(mansLogs,text='+',padx='40',pady='20',command=lambda:btnCommand('+'))
-btnAtnem=Button(mansLogs,text='-',padx='40',pady='20',command=lambda:btnCommand('-'))
-btnReiz=Button(mansLogs,text='*',padx='40',pady='20',command=lambda:btnCommand('*'))
-btnDalit=Button(mansLogs,text='/',padx='40',pady='20',command=lambda:btnCommand('/'))
-btnVienadibas=Button(mansLogs,text='=',padx='40',pady='20',command=Vienads) 
-btnC=Button(mansLogs,text='C',padx='40',pady='20',command=notirit)
+btn0=Button(mansLogs,text='0',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(0))
+btn0=Button(mansLogs,text='0',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(0))
+btn1=Button(mansLogs,text='1',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(1))
+btn2=Button(mansLogs,text='2',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(2))
+btn3=Button(mansLogs,text='3',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(3))
+btn4=Button(mansLogs,text='4',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(4))
+btn5=Button(mansLogs,text='5',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(5))
+btn6=Button(mansLogs,text='6',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(6))
+btn7=Button(mansLogs,text='7',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(7))
+btn8=Button(mansLogs,text='8',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(8))
+btn9=Button(mansLogs,text='9',padx='40',pady='20',bg= 'blue',font=('Arial Black',20),bd = 10,command=lambda:btnClick(9))
+btnSask=Button(mansLogs,text='+',padx='40',pady='20',bg= 'green',font=('Arial Black',20),bd = 10,command=lambda:btnCommand('+'))
+btnAtnem=Button(mansLogs,text='-',padx='40',pady='20',bg= 'green',font=('Arial Black',20),bd = 10,command=lambda:btnCommand('-'))
+btnReiz=Button(mansLogs,text='*',padx='40',pady='20',bg= 'green',font=('Arial Black',20),bd = 10,command=lambda:btnCommand('*'))
+btnDalit=Button(mansLogs,text='/',padx='40',pady='20',bg= 'green',font=('Arial Black',20),bd = 10,command=lambda:btnCommand('/'))
+btnVienadibas=Button(mansLogs,text='=',padx='40',pady='20',bg= 'green',font=('Arial Black',20),bd = 10,command=Vienads) 
+btnC=Button(mansLogs,text='C',padx='40',pady='20',bg= 'red',font=('Arial Black',20),bd = 10,command=notirit)
 
 e=Entry(mansLogs,width=15,bd=10,font=("Arial Black",20),justify="right")
 
